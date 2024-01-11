cffd4b7bce4150a7fa66aa77026ebf3bc4ad1794
paulgioeli1 <104484028+paulgioeli1@users.noreply.github.com>
2024-01-11 09:32:26 -0800

    Add files via upload

---
+++ JavaPractice.java
@@ -0,0 +1,27 @@
+package javapractice;
+
+/**
+ *
+ * @author paulgioeli
+ */
+public class JavaPractice {
+
+    public static int Addem(int num1, int num2){
+            return num1 + num2;
+        }
+    
+    public static void main(String[] args) {
+        
+        int var1 = 4;
+        int var2 = 7;
+        
+        int var3 = var1 * var2;
+        
+        int var4 = Addem(var1, var2);
+        
+        System.out.println(var4);
+    }
+    
+   
+    
+}
